35c3f374c364b9e65327456471695b53d1e050a3
wangd1230 <790894413@qq.com>
2016-11-30 10:09:43 +0800

    third

--- js/list.js
+++ js/list.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
         list.set("name",name);
         list.set("number",num);
         list.save().then(function() {
+            alert('登记成功');
             window.location.href = "../index.html";
             }, function(error) {
             alert(JSON.stringify(error));
@@ -15,8 +16,15 @@ $(document).ready(function(){
     }
     $(function() {
     $(".btn").click( function(e) {
+        var x = String($('#inputName').val()).length;
+        var y = String($('#inputNumber').val()).length;
+        if (x>0&&y>0) {
       e.preventDefault();
       releaseNewProduct();
+  }
+  else{
+    alert("请填写完表单");
+  }
     });
 });
 })
